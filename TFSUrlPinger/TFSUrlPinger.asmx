38092bf486c5cdcd307f454e94baf0fbf52a5051
Michael Letterle <michael.letterle@gmail.com>
2010-07-23 23:16:35 -0400

    Initial Commit

---
+++ TFSUrlPinger/TFSUrlPinger.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="TFSUrlPinger.asmx.cs" Class="TFSUrlPinger.TFSUrlPinger" %>
---
+++ TFSUrlPinger/TFSUrlPinger.asmx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+
+namespace TFSUrlPinger
+{
+    /// <summary>
+    /// Summary description for Service1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]    
+    public class TFSUrlPinger : System.Web.Services.WebService
+    {
+        [SoapDocumentMethod("http://schemas.microsoft.com/TeamFoundation/2005/06/Services/Notification/03/Notify",
+                            RequestNamespace = "http://schemas.microsoft.com/TeamFoundation/2005/06/Services/Notification/03")]
+        [WebMethod]
+        public void Notify(string eventXml)
+        {
+            foreach (string url in System.Configuration.ConfigurationManager.AppSettings["PingUrls"].Split(','))
+            {
+                try
+                {
+                    System.Net.WebRequest req = System.Net.HttpWebRequest.Create(url);
+                    req.GetResponse();
+                }
+                catch
+                {
+                }
+            }
+        }
+    }
+}
